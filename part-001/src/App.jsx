c1828b21b73532da95235eecae47918a1cdf65b2
abrazax56 <ahmadbenirusli@gmail.com>
2024-01-13 08:45:02 +0700

    part-001

---
+++ part-001/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="items-center min-h-screen flex justify-center align-middle bg-blue-600 text-white font-bold">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
